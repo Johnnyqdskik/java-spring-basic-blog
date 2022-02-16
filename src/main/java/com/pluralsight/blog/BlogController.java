dee8b75906b9bea94ff3f1fed49bda52fa1ef21f
Nikola Stanić <nibscompany@gmail.com>
2022-02-16 23:00:22 +0100

    Creating Controller that displays ThymeLeaf Template

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,23 @@
 package com.pluralsight.blog;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
 public class BlogController {
+
+    @GetMapping("thyme")
+    public String listPosts(Map<String, Object>model) {
+        model.put ("title", "Blog Post 1");
+        return "home";
+    }
+
+
 
 }
