97a54972486013d2b1d93ad88f41e234df4c7a55
Nikola Stanić <nibscompany@gmail.com>
2022-02-17 20:24:34 +0100

    New changes

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,20 +1,19 @@
 package com.pluralsight.blog;
 
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
 
-@RestController
-@RequestMapping("/")
+
+
+@Controller
 public class BlogController {
 
-    @GetMapping("thyme")
-    public String listPosts(Map<String, Object>model) {
-        model.put ("title", "Blog Post 1");
+    @RequestMapping("/")
+    public String listPosts(ModelMap modelMap) {
+        modelMap.put ("title", "Blog Post 1");
         return "home";
     }
 
