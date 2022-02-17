fdb43d5945167857416af0ad9f4d26e255404604
Nikola Stanić <nibscompany@gmail.com>
2022-02-17 21:39:35 +0100

    Changes on PostRepository

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,6 +1,7 @@
 package com.pluralsight.blog;
 
 
+import com.pluralsight.blog.data.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,10 @@ public class BlogController {
         return "home";
     }
 
+    private PostRepository postRepository = new PostRepository();
 
-
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
 }
+
